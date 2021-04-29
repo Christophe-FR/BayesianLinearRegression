12bbfc6e0b19f5d23abeab64a72b4896f5468795
Christophe <lutz.christophe@gmail.com>
2021-04-29 19:22:02 +0200

    ready to go online

--- bayesian_linear_regression.py
+++ bayesian_linear_regression.py
@@ -5,7 +5,7 @@ Created on Thu Mar 18 18:59:36 2021
 @author: lutzc
 """
 
-#streamlit run linear_bayesian_regression.py
+#streamlit run bayesian_linear_regression.py
 import base64
 import matplotlib
 from sympy import *
@@ -17,27 +17,34 @@ import plotly.figure_factory as ff
 import plotly.express as px
 import os
 
-#init_printing()
-    
+
+
 def get_table_download_link_csv(df):
     csv = df.to_csv().encode()
     b64 = base64.b64encode(csv).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="weights.csv" target="_blank">Download as csv file</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="weights.csv" target="_blank">Download table as csv</a>'
     return href    
 
+
+
 st.title('Bayesian Linear Regression')
-st.write('If there is one algorithm that rules the world it is surely the least square regression. It is literally everywhere on :earth_americas:! Finance, science and technology use it extensively to predict, model and win the race of acute systems. The straight line fitting is commonly used by the average people but many more variants are living out there and are getting increasingly popular with the raise of Artificial Intelligence. In this application you will be able to experiment with linear least square which is both powerful and elegant. Under the bayesian framework the following can be naturally embraced collectively:')
-st.write('* maximum likelihood (least square)')   
-st.write('* maximum a posteriory (regularized least square)')
-st.write('* posterior predictive')  
-st.write('The data uncertainty is assumed to be gaussian, and equal everywhere (homoscedastic).')
+
+st.write('If there is one algorithm that has experienced massive adoption accross all domains, it is surely the least square regression. It is literally everywhere on :earth_americas:! Finance, science and technology use it extensively to predict, model and win the race of high-accuracy systems. The straight line fitting is commonly the entry point of the field but many more variants are living out there and are getting increasingly popular with the raise of Artificial Intelligence.')
+
+st.write('In this application you will be able to experiment with linear models which are both powerful and elegant. A linear model $\mathcal{M}$  attempts to explain the target $y$ from a weighted linear combination of the input features $(x_j)_{1..M}$ with the weights $(w_j)_{1..M}$. The general form of a linear model is as follow:')
+
+st.write(r'$\mathcal{M} : y = \sum{w_jx_j} + \epsilon$ where $\epsilon$ is a 0-mean gaussian noise of precision $\beta=\frac{1}{\sigma^2}$.')
+
+st.write('The goal in the following is to determine the values of weights.')
+
+
 
 st.header('Load data')
-st.write('Firstly we need to set the stage of the problem. What we would like to predict? Based on what? Those are respectively called the target feature (y) and the input features (x,...).')
-datasource = st.radio('',('From CSV', 'From Example'))
+
+st.write('Import your data ($y$ shall be the last column and $x$ the next to last) or use a predefined data set.')
+datasource = st.radio('Source',('From CSV', 'From Example'))
 
 if datasource == 'From CSV' :
-    st.write('Drop here the csv file containing the input and output features')
     file = st.file_uploader("Upload Files",type=['csv'])
     if file is not None:
         df = pd.read_csv(file, delimiter = ';')
@@ -48,10 +55,9 @@ else:
             {'x': np.array([1,2,3,4,5]),
              'y': np.array([2,5,10,17,26] + np.array([-0.008046  ,  0.00028632,  0.00710868,  0.00677746,  0.00552161])
              )})
-#file = 'example.csv'
 
-#
 
+    
 if df is not None:
     df
     x = df.iloc[:,-2].to_numpy()
@@ -62,13 +68,16 @@ if df is not None:
     fig = px.scatter(x = x, y = y)
     fig.update_layout(xaxis_title= xlabel, yaxis_title= ylabel)
     fig
+    
+    
     st.header('Build additional features')
-    st.write('Here the opportunity is given to build extra input features before rushing to the regression. Why would you to that? The reason is to increase the model complexity in order to more accurately predict the target feature. The functions to derive these features are called kernels and can be of all nature.')
-    fun_str = st.text_input('List kernel functions (separated by commas) here - for example: x**2, sin(x), sqrt(x), 1 (for constant)', value='1')
+    st.write('This section offers the opportunity to build extra input features with custom kernels functions applied on the loaded data in order to boost the model representational capability.')
+    fun_str = st.text_input('List of kernel functions (separated by commas) - for example: x**2, sin(x), sqrt(x), 1', value='1')
     if fun_str != '':
         for fun in fun_str.split(','):
             df.insert(loc=0, column=fun, value=df.eval(fun))
     df
+    
     
     
     X = df.iloc[:,0:-1].to_numpy()
@@ -77,11 +86,15 @@ if df is not None:
     M = X.shape[1]
     eigvalues, _  = np.linalg.eig(XTX)
     
+    
+    
+    
     st.header('Hyperparameters tuning')
-    st.write('Bias or variance? That is the tradeoff. Hyperparameters are here to fine tune your models. Herein can be adjusted:')
-    st.write('* Alpha which is the precision (or inverse variance) of the weights gaussian prior') 
-    st.write('* Beta which is the precision (or inverse variance) of the data gaussian likelihood') 
-    st.write('These two guys can be trained automatically by optimizing the data evidence in order for the model to be the most reasonanle in the light of the data. By the way the ratio alpha/beta is often called the regularization parameter, or lambda.')
+    st.write('Bias or variance? That is the tradeoff. Hyperparameters are here to fine tune your model. Herein can be adjusted:')
+    st.write(r'- $\alpha$ which is the precision (or inverse variance)  of the weights prior.') 
+    st.write(r'- $\beta$ which is the precision (or inverse variance) of the targets.') 
+    st.write(r'Note that the ratio $\alpha/\beta$ is called  regularization parameter, or $\lambda$.')
+    
     
     eps = np.finfo(float).eps
     alpha_trainable = st.checkbox('Make Alpha trainable',True)
@@ -90,9 +103,7 @@ if df is not None:
     beta_trainable = st.checkbox('Make Beta trainable',True)
     beta = eps + st.slider(label = 'Beta' + beta_trainable * '_0', value = 1.0, min_value = 0.0, max_value = 100.0, step = 0.5)
     
-    st.write('The variable gamma is derived from alpha, beta and the data only.')
-    
-    hpdf = pd.DataFrame({'alpha': [alpha], 'beta': [beta], 'gamma': [gamma]})
+    hpdf = pd.DataFrame({'alpha': [alpha], 'beta': [beta], 'gamma': [gamma], 'lambda': [alpha/beta]})
     
     thres = 1e-6
     for _ in range(1000):
@@ -107,17 +118,17 @@ if df is not None:
             alpha = gamma / np.dot(mN.T, mN)
         if beta_trainable:
             beta = 1.0 / ( np.sum(residual**2) / (N - gamma) )
+        lmbda = alpha/beta
         if np.abs(alpha - hpdf['alpha'].iloc[-1]) < thres and np.abs(beta - hpdf['beta'].iloc[-1]) < thres:
             break;
-        hpdf = hpdf.append({'alpha': alpha, 'beta': beta, 'gamma': gamma},ignore_index=True)
-        
+        hpdf = hpdf.append({'alpha': alpha, 'beta': beta, 'gamma': gamma, 'lambda': alpha/beta},ignore_index=True)     
     hpdf
     
     sigmaN = 1/beta + np.diag(np.dot(X,np.dot(SN,X.T)))
     muN = np.dot(X, mN )
     
     st.header('Results')
-    st.write('By the power of bayesian regression, not only a maximum a posteriory estimate of the weights are available but also an estimate of their uncertainties. Consequently the prediction is also uncertain: part of the uncertainty originates from the model and part of uncertainty originates from the noise of the data.')
+    st.write('The plot below shows the prediction with the maximum a posteriori weights, and the posterior predictive distribution which indicates the uncertainty related to both the model and the noise of targets.')
     
     red = 'rgba(255, 0, 0, 0.3)'
     fig = go.Figure()
@@ -156,6 +167,47 @@ if df is not None:
     weights
     
     st.markdown(get_table_download_link_csv(weights), unsafe_allow_html=True)
-st.write('Based on Bishop, Christopher. (2006). Pattern Recognition and Machine Learning. 10.1117/1.2819119.')
+
+
+
+
+st.write('')
+st.write('')
+st.write('')
+
+
+
+
+st.write('---')
+if st.button("Learn More about Bayesian Regression"):
+    st.write('Bayesian regression is a very powerful framework at the origin of least square and regularized least square fitting methods. The hearth of the reasoning is the Bayes formula:')
+
+    st.write(r'$P(W|Y) = \frac{P(Y|W)P(W)}{P(Y)}$')
+
+    st.write('Conditionning every probabilities above on $X$ and $\mathcal{M}$ yields the main formula of interest:')
+
+    st.write(r'$P(W|Y,X,\mathcal{M}) = \frac{P(Y|W,X,\mathcal{M})P(W|X,\mathcal{M})}{P(Y|X,\mathcal{M})}$')
+
+    st.write('where $Y = [y_i]$, $X = [x_{i,j}]$ and $W = [w_j]$ are respectively the targets, the features and the weights matrices')
+    
+    st.write('Let us analyze each terms:')
+    st.write(r'- $P(W|X,\mathcal{M})$ is the weight prior, which is taken as a 0-mean gaussian distribution of precision parameter $\alpha$ herein (it does not depend on $X$).')
+    st.write(r'- $P(Y|W,X,\mathcal{M})$ is the likelihood, which in the light of the model $\mathcal{M}$ is a $XW$-mean diagonal gaussian of precision $\beta$.')
+    st.write(r'- $P(Y|X,\mathcal{M})$ is the model evidence which indicates how likely the model was to generate the target values samples, when marginalizing the weights.')
+    st.write(r'- $P(W|Y,X,\mathcal{M})$ is the weights posterior probability distribution.')
+    st.write(r'Maximizing (w.r.t. $W$) the probability $P(W|Y,X,\mathcal{M})$ yields the MAP weights. Marginalizing (w.r.t. $W$) the model prediction form $\mathcal{M}$ yields the measure of the uncertainty in the prediction. ')
+#    st.write(r'Maximizing (w.r.t. $W$) $P(Y|X,\mathcal{M})$  weights combination (actually a full continuous set) describing every possible models of the form of $\mathcal{M}$.')
+    st.write(r'The $\alpha$ and $\beta$ hyperparameter can be set automatically by maximizing the model evidence. Refer to the following book for more details: ')
+    st.write(':green_book: Bishop, Christopher. (2006). Pattern Recognition and Machine Learning. 10.1117/1.2819119.')
+    
+    
+    
+st.write('---')
+
+
+
 st.write('Provided as is without any warranty.')
 st.write('Created by Lutz Christophe - lutz.christophe@gmail.com - April 2021')
+
+
+
